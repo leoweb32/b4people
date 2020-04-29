46309435df2a25357d89719344a8349f042e9717
leoweb32 <leoweb32@gmail.com>
2020-04-29 15:31:19 -0300

    estrutura da pagina sobre

--- page-sobre.php
+++ page-sobre.php
@@ -1,5 +1,99 @@
 
 <?php 
-/* template name:sobre */
+/* template name:Sobre a B4People */
 get_header();
 ?>
+<div id="title_serv">
+    <?php the_title ?>
+</div>
+<section id="quem_somos">
+    <div id="text_quem"></div>
+    <figure class="figure_quem">
+        <img src="" alt="">
+    </figure>
+</section>
+
+<section id="oque_fazemos">
+    <figure class="figure_oque">
+        <img src="" alt="">
+    </figure>
+    <div id="textoque"></div>
+</section>
+
+<section id="servicos">
+    <h2>Como fazemos</h2>
+    <p>Soluções estruturadas e sistêmicas personalizadas e alinhadas aos
+objetivos de inclusão, cultura e inovação da sua empresa.</p>
+
+    <ul>
+        <li>
+            <figure>
+            </figure>
+            <h3></h3>
+            <p></p>
+            <button></button>
+        </li>
+        <li>
+            <figure>
+            </figure>
+            <h3></h3>
+            <p></p>
+            <button></button>
+        </li>
+        <li>
+            <figure>
+            </figure>
+            <h3></h3>
+            <p></p>
+            <button></button>
+        </li>
+        <li>
+            <figure>
+            </figure>
+            <h3></h3>
+            <p></p>
+            <button></button>
+        </li>
+    </ul>
+</section>
+
+<section id="sobre_ana">
+    <div class="container">
+        <div id="text_ana">
+                <h3></h3>
+                <h4></h4>
+                <p></p>
+                <button></button>
+            </div>
+            <img src=""> 
+    </div>
+
+</section>
+
+<section id="depoimentos">
+    <div id="clientes"></div>
+</section>
+
+<section id="equipe">
+    <div id="text_equipe">
+    </div>
+    <div id="equipe_list">
+        <div id="equipe_item">
+            <img src="">
+            <span class="label_name"></span>
+        </div>
+        <div id="equipe_item">
+            <img src="">
+            <span class="label_name"></span>
+        </div>
+        <div id="equipe_item">
+            <img src="">
+            <span class="label_name"></span>
+        </div>
+        <div id="equipe_item">
+            <img src="">        
+            <span class="label_name"></span>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
